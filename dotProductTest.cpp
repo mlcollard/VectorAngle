8f7793834f64fea2e7a17b0ad8c42d00879e41f1
Michael L. Collard <collard@uakron.edu>
2023-10-24 06:15:39 -0400

    Add initial files

---
+++ dotProductTest.cpp
@@ -0,0 +1,17 @@
+/*
+    dotProductTest.cpp
+
+    Test program for the dotProduct()
+*/
+
+#include <vector>
+#include <cassert>
+#include "dotProduct.hpp"
+
+// Make sure assert() is not removed
+#undef NDEBUG
+
+int main() {
+
+    return 0;
+}
